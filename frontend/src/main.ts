2c308c502d57c00af352c9f904e4473f719ed51b
1DECRATE1 <nukegame12@gmail.com>
2025-06-20 16:00:29 +0300

    add auth

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -12,22 +12,76 @@ export const ctx = canvas.getContext("2d")!;
 
 function isLogin() {
   if (window.localStorage.getItem("token") !== null) {
-    gameButton.style.visibility = "auto";
+    gameButton.style.display = "block";
+    loginDiv!.style.display = "none";
+    return;
   }
-  gameButton.style.visibility = "hidden";
+  gameButton.style.display = "none";
+  return;
 }
-
+let signUpIsShown: boolean = true;
 const signUp = document.getElementById("signUp");
 const signIn = document.getElementById("signIn");
+const inputContainer = document.getElementById("inputContainer");
+const usernameInput: HTMLInputElement = document.createElement("input");
+const passwordInput: HTMLInputElement = document.createElement("input");
+usernameInput.id = "usernameInput";
+passwordInput.id = "passwordInput";
+usernameInput.placeholder = "username";
+passwordInput.placeholder = "password";
+
+usernameInput.type = "text";
+passwordInput.type = "password";
+
+usernameInput.addEventListener("change", (e) => {
+  e.preventDefault();
+  usernameInput.value = (e.target as HTMLInputElement).value;
+});
+passwordInput.addEventListener("change", (e) => {
+  e.preventDefault();
+  passwordInput.value = (e.target as HTMLInputElement).value;
+});
+inputContainer?.appendChild(usernameInput);
+inputContainer?.appendChild(passwordInput);
+const loginDiv = document.getElementById("loginDiv");
+const submitButton = document.getElementById("submitButton");
+const submitFunction = async () => {
+  const url = signUpIsShown
+    ? "http://localhost:3000/api/user/create"
+    : "http://localhost:3000/api/user/login";
+  const body = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: usernameInput.value,
+      password: passwordInput.value,
+    }),
+  });
+  const data = await body.json();
+  const token = data.access_token;
+  localStorage.setItem("token", token);
+};
+submitButton!.onclick = (e) => {
+  e.preventDefault();
+  submitFunction();
+};
+
 signIn?.addEventListener("click", () => {
-  console.log(1);
   signIn.style.display = "none";
   signUp!.style.display = "block";
+  usernameInput.value = "";
+  passwordInput.value = "";
+  signUpIsShown = false;
 });
 
 signUp?.addEventListener("click", () => {
   signIn!.style.display = "block";
   signUp!.style.display = "none";
+  usernameInput.value = "";
+  passwordInput.value = "";
+  signUpIsShown = true;
 });
 
 export const game = new Game({ ctx, canvas });
